0bebf862594bdbc8030e1ed2649f91e46dd66498
Kim <kimbaricog4@gmail.com>
2026-01-30 22:04:38 +0800

    m

--- frameform/src/App.jsx
+++ frameform/src/App.jsx
@@ -1,14 +1,12 @@
 import "./App.css";
 import Header from "./components/Header.jsx";
 import Hero from "./components/Hero.jsx";
-import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <Header></Header>
       <Hero></Hero>
-      <Analytics />
     </>
   );
 }
