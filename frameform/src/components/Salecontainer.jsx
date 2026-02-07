2be0a6c26223529f9961d1f6490a2d8798e39902
Kim <kimbaricog4@gmail.com>
2026-02-07 14:41:27 +0800

    remove snow

--- frameform/src/components/Salecontainer.jsx
+++ frameform/src/components/Salecontainer.jsx
@@ -20,13 +20,12 @@ function Salecontainer() {
           <Sale_offer img={Encoding} text="Encoding" />
         </div>
       </div>
-
-      <Snowfall
+      {/*   <Snowfall
         color="white"
         snowflakeCount={100} // number of snowflakes
         radius={[1, 5]} // min & max size
         speed={[0.5, 2]} // min & max falling speed
-      />
+      />*/}
     </>
   );
 }
